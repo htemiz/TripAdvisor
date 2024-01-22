295ae5c6e45d72cdafce8090dedc03755e65cb9a
Hakan Temiz <htemiz@artvin.edu.tr>
2024-01-22 17:31:18 +0300

    Initial commit

--- otel_yorumlarini topla.bat
+++ otel_yorumlarini topla.bat
@@ -1,6 +1,6 @@
 echo C:\Users\Pc\AppData\Local\Programs\Python\Python36\python.exe "get_hotel_info_and_reviews.py"
 
-python.exe "get_hotel_info_and_reviews.py"
+C:\Users\Pc\AppData\Local\Programs\Python\Python39\python.exe "parse_hotels_with_reviews.py" Hotels_in_Florida_Region.parquet Florida
 
 pause
 
